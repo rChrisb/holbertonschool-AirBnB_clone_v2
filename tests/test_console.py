96766a8a7ee35d536dafc114ccc7876f860375e5
rChrisb <5767@holbertonstudents.com>
2023-03-20 15:38:46 +0100

    test the output in console, when creating object with specific id

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 """test for the console"""
+
+import unittest
+from console import HBNBCommand
+import sys
+import io
+
+
+class Test(unittest.TestCase):
+    def test_create_instance(self):
+        """tests the output when giving a specific id at creation"""
+
+        output = io.StringIO()
+        sys.stdout = output
+        console = HBNBCommand()
+        console.onecmd("create City id='hello_Antoine'")
+        sys.stdout = sys.__stdout__
+        expected_output = output.getvalue()
+        self.assertIn("hello Antoine", expected_output)
