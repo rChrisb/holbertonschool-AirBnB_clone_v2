1986bdc57686b55f2d52007e5b77b6c5f72c0c22
rChrisb <5767@holbertonstudents.com>
2023-03-21 17:41:04 +0100

    Revert "DBStorage - Review"
    This reverts commit 58ba1f57ac3794b65e5767fa9706d352f863050a.

--- models/place.py
+++ models/place.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
-from os import getenv
-from sqlalchemy.orm import relationship
-import models
-from models.review import Review
 
 
 class Place(BaseModel):
     """ A place to stay """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", backref="place")
-    else:
-        @property
-        def reviews(self):
-            reviews_list = []
-            for review in models.storage.all(Review).values():
-                if self.id == review.place_id:
-                    reviews_list.append(review)
-            return reviews_list
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
