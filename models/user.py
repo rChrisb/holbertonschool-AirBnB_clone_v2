e84e516cbdd171abcd74d504d8abfa14384f0b0c
AntoineJacob <5646@holbertonstudents.com>
2023-03-21 08:19:25 -0700

    delete the change of task 8

--- models/user.py
+++ models/user.py
@@ -2,7 +2,6 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -12,4 +11,3 @@ class User(BaseModel, Base):
     password = Column('password', String(128), nullable=False)
     first_name = Column('first_name', String(128), nullable=True, default="NULL")
     last_name = Column('last_name', String(128), nullable=True, default="NULL")
-    places = relationship("Place", cascade="delete", backref="user")
