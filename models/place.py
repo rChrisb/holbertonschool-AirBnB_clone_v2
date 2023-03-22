4c7a6ddc6670af1a94cd0e3d55fa98196b5a47c6
AntoineJacob <5646@holbertonstudents.com>
2023-03-22 07:12:03 -0700

    last chance for task 9

--- models/place.py
+++ models/place.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForeignKey, Integer, Float, Table
+from models.review import Review
+from sqlalchemy import Column, ForeignKey, String, Integer, Float
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+    reviews = relationship("Review", backref="place", cascade="delete")
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            list_review = []
+            for rev in list(models.storage.all(Review).values()):
+                if rev.place_id == self.id:
+                    list_review.append(rev)
+            return list_review
