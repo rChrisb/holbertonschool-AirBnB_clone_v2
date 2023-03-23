7714b9236860d53dca2e4022c349d60f3606f742
rChrisb <5767@holbertonstudents.com>
2023-03-23 12:28:30 +0100

    DBStorage - Amenity

--- models/place.py
+++ models/place.py
@@ -33,7 +33,7 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship(
-        "Amenity", secondary="place_amenity")
+        "Amenity", secondary="place_amenity", viewonly=False)
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
 
