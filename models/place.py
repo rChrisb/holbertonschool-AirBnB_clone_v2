be656ebfc5478dd52d6bce0c551b83e2f5bceb81
rChrisb <5767@holbertonstudents.com>
2023-03-23 15:50:51 +0100

    DBStorage - Amenity

--- models/place.py
+++ models/place.py
@@ -9,14 +9,13 @@ from os import getenv
 import models
 
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    place_amenity_table = Table("place_amenity", Base.metadata,
-                                Column("place_id", String(60),
-                                       ForeignKey("places.id"),
-                                       primary_key=True, nullable=False),
-                                Column("amenity_id", String(60),
-                                       ForeignKey("amenities.id"),
-                                       primary_key=True, nullable=False))
+place_amenity_table = Table("place_amenity", Base.metadata,
+                            Column("place_id", String(60),
+                                   ForeignKey("places.id"),
+                                   primary_key=True, nullable=False),
+                            Column("amenity_id", String(60),
+                                   ForeignKey("amenities.id"),
+                                   primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
