afd6457366d5a274e096d27b49a6f794fa46276d
rChrisb <5767@holbertonstudents.com>
2023-03-23 14:18:17 +0100

    Update tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,7 +12,7 @@ class test_fileStorage(unittest.TestCase):
     def setUp(self):
         """ Set up test environment """
         del_list = []
-        for key in storage._FileStorage__objects.keys():
+        for key in storage.all().keys():
             del_list.append(key)
         for key in del_list:
             del storage._FileStorage__objects[key]
