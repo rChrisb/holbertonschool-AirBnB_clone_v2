d615f68363585ad65910727e1c2ea7a4bb5643d9
rChrisb <5767@holbertonstudents.com>
2023-03-23 10:54:22 +0100

    DBStorage - Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -7,6 +7,7 @@ from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
+    """Amenities for places"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity")
--- models/place.py
+++ models/place.py
@@ -9,12 +9,13 @@ from os import getenv
 import models
 
 
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", ForeignKey("places.id"),
-                             String(60), primary_key=True, nullable=False
-                             ),
-                      Column("amenity_id", ForeignKey("amenities.id"),
-                             String(60), primary_key=True, nullable=False))
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
