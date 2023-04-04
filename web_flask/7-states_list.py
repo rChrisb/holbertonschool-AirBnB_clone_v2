c4626549d42d66d52f83395124f36f6ec4ff81bf
rChrisb <5767@holbertonstudents.com>
2023-04-04 14:15:59 +0200

    List of states in alphabetic order

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,12 +10,11 @@ from models.state import State
 app = Flask(__name__)
 
 
-
-
 @app.route('/states_list', strict_slashes=False)
 def all_states():
     all_states = storage.all(State)
     return render_template("7-states_list.html", states=all_states)
+
 
 @app.teardown_appcontext
 def teardown_handle(app):
